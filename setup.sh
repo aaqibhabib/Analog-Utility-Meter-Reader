cfff2b9658a5f8762a11da4f6651b6d64db81d32
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2017-02-04 19:52:19 -0500

    setup fix

--- setup.sh
+++ setup.sh
@@ -40,7 +40,7 @@ git clone https://github.com/opencv/opencv_contrib.git
 cd opencv
 mkdir build
 cd build
-cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
+cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON -DENABLE_PRECOMPILED_HEADERS=OFF -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
 make -j4
 sudo make install
 sudo ldconfig
